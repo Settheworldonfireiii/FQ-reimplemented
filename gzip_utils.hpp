0d4885baa6438922ef87b1cee927b9c7ec295213
Bo Li <li.bo@gene.com>
2022-06-20 11:37:33 -0400

    Test passed

--- gzip_utils.hpp
+++ gzip_utils.hpp
@@ -1,5 +1,3 @@
-
-
 #ifndef GZIP_UTILS
 #define GZIP_UTILS
 
@@ -8,163 +6,175 @@
 #include <string>
 #include <cstdio>
 
-#include "izlib.h"
-//#include <zlib.h>
-#include "kseq.h"
+#include "external/slw287r_trimadap/izlib.h"
+#include "external/kseq.h"
 #include "compress.hpp"
 
 
 struct Read {
-    std::string name, comment, seq, qual; // qual.empty() -> FASTA reads
+	std::string name, comment, seq, qual; // qual.empty() -> FASTA reads
 
-    size_t size() const {
-        return name.length() + (comment.empty() ? 0 : 1 + comment.length()) + seq.length() + 3 + (qual.empty() ? 0 : 3 + qual.length());
-    }
+	size_t size() const {
+		return name.length() + (comment.empty() ? 0 : 1 + comment.length()) + seq.length() + 3 + (qual.empty() ? 0 : 3 + qual.length());
+	}
 
-    std::string toString() const {
-        return "@" + name + (comment.empty() ? "" : (" " + comment)) + "\n" + seq + "\n" + (qual.empty() ? "" : "+\n" + qual + "\n");
-    }
+	std::string toString() const {
+		return "@" + name + (comment.empty() ? "" : (" " + comment)) + "\n" + seq + "\n" + (qual.empty() ? "" : "+\n" + qual + "\n");
+	}
 };
 
+KSEQ_INIT(gzFile, gzread);
+
 struct iGZipFile {
-    KSEQ_INIT(gzFile, gzread);
-
-    gzFile sequence_file = NULL;
-    kseq_t* sequence_kseq = NULL;
-
-
-    iGZipFile(const std::string& input_file) {
-        sequence_file = gzopen(input_file.c_str(), "r");
-        if (sequence_file == NULL) {
-            printf("Cannot find sequence file %s!\n", input_file.c_str());
-            exit(-1);
-        }
-        sequence_kseq = kseq_init(sequence_file);
-    }
-
-    ~iGZipFile() {
-        kseq_destroy(sequence_kseq);
-        gzclose(sequence_file);
-        sequence_kseq = NULL;
-    }
+	std::string input_file_;
+	gzFile sequence_file = nullptr;
+	kseq_t* sequence_kseq = nullptr;
 
 
-    bool next(Read& aread) {
-        int length = kseq_read(sequence_kseq);
+	iGZipFile(const std::string& input_file) : input_file_(input_file) {
+		sequence_file = gzopen(input_file_.c_str(), "r");
+		if (sequence_file == nullptr) {
+			printf("Cannot find sequence file %s!\n", input_file_.c_str());
+			exit(-1);
+		}
+		sequence_kseq = kseq_init(sequence_file);
+	}
 
-        if (length > 0) {
-            aread.name = sequence_kseq->name.s;
-            aread.comment = sequence_kseq->comment.s;
-            aread.seq = sequence_kseq->seq.s;
-           // aread.qual = sequence_kseq->is_fastq ? sequence_kseq->qual.s : "";
-            aread.qual = sequence_kseq->qual.s;
-        }
-        else if (length == -1)
-            return false; // End of file
-        else if (length == -3) {
-            printf("Error reading stream; didn't reach the end of sequence file, which might be corrupted!\n");
-            exit(-1);
-        }
-        else if (length == -2) {
-            printf("Truncated quality string!\n");
-            exit(-1);
-        }
-        else {
-            assert(length == 0);
-            printf("Detected a read with 0 sequence length!\n");
-            exit(-1);
-        }
+	iGZipFile(iGZipFile&& o) {
+		input_file_ = std::move(o.input_file_);
+		sequence_file = o.sequence_file;
+		sequence_kseq = o.sequence_kseq;
+		o.sequence_file = nullptr;
+		o.sequence_kseq = nullptr;
+	}
 
-        return true;
-    }
+	~iGZipFile() {
+		if (sequence_kseq != nullptr) kseq_destroy(sequence_kseq);
+		if (sequence_file != nullptr) gzclose(sequence_file);
+	}
 
 
-    bool next(std::string& line) { // Only get one line
-        int ret = ks_getuntil(sequence_kseq->f, KS_SEP_LINE, &sequence_kseq->comment, 0);
+	bool next(Read& aread) {
+		int length = kseq_read(sequence_kseq);
+		if (length > 0) {
+			aread.name = sequence_kseq->name.s;
+			aread.comment = sequence_kseq->comment.s;
+			aread.seq = sequence_kseq->seq.s;
+			aread.qual = sequence_kseq->is_fastq ? sequence_kseq->qual.s : "";
+		}
+		else if (length == -1) 
+			return false; // End of file
+		else if (length == -3) {
+			printf("File %s: error reading stream; didn't reach the end of sequence file, which might be corrupted!\n", input_file_.c_str());
+			exit(-1);			
+		}
+		else if (length == -2) {
+			printf("File %s: truncated quality string!\n", input_file_.c_str());
+			exit(-1);			
+		}
+		else {
+			assert(length == 0);
+			printf("File %s: Detected a read with 0 sequence length!\n", input_file_.c_str());
+			exit(-1);
+		}
 
-        if (ret == -3) {
-            printf("Error reading stream in next(line)!\n");
-            exit(-1);
-        }
+		return true;
+	}
 
-        if (ret >= 0) {
-            line = sequence_kseq->comment.s;
-            return true;
-        }
 
-        return false;
-    }
+	bool next(std::string& line) { // Only get one line
+		int ret = ks_getuntil(sequence_kseq->f, KS_SEP_LINE, &sequence_kseq->comment, 0);
+
+		if (ret == -3) {
+			printf("File %s: error reading stream in next(line)!\n", input_file_.c_str());
+			exit(-1);						
+		}
+
+		if (ret >= 0) {
+			line = sequence_kseq->comment.s;
+			return true;
+		}
+
+		return false;
+	}
 };
 
 
 struct oGZipFile {
-    FILE *fo;
-    Compressor *compressor;
+	FILE *fo = nullptr;
+	Compressor *compressor = nullptr;
 
-    oGZipFile(const std::string& output_file, int num_threads = 1, size_t buffer_size = compressor_buffer_size, int compression_level = 6) {
-        fo = fopen(output_file.c_str(), "wb");
-        if (fo == NULL) {
-            printf("Cannot creat output file %s!\n", output_file.c_str());
-            exit(-1);
-        }
+	oGZipFile(const std::string& output_file, int num_threads = 1, size_t buffer_size = compressor_buffer_size, int compression_level = 6) {
+		fo = fopen(output_file.c_str(), "wb");
+		if (fo == nullptr) {
+			printf("Cannot creat output file %s!\n", output_file.c_str());
+			exit(-1);
+		}
 
-        assert(num_threads >= 1);
-        compressor = NULL;
-        if (num_threads == 1) {
-            compressor = new SingleThreadCompressor(buffer_size, compression_level);
-        }
-        else {
-            compressor = new MultiThreadsCompressor(num_threads, buffer_size, compression_level);
-        }
-    }
+		assert(num_threads >= 1);
+		compressor = nullptr;
+		if (num_threads == 1) {
+			compressor = new SingleThreadCompressor(buffer_size, compression_level);
+		} 
+		else {
+			compressor = new MultiThreadsCompressor(num_threads, buffer_size, compression_level);
+		}
+	}
 
-    ~oGZipFile() {
-        close();
-    }
+	oGZipFile(oGZipFile&& o) {
+		fo = o.fo;
+		compressor = o.compressor;
+		o.fo = nullptr;
+		o.compressor = nullptr;
+	}
 
-    void close() {
-        if (fo != NULL) {
-            flush();
-            fclose(fo);
-            delete compressor;
-            fo = NULL;
-            compressor = NULL;
-        }
-    }
+	~oGZipFile() {
+		close();
+	}
 
-    void flush() {
-        size_t cprs_size = compressor->compress();
-        if (cprs_size > 0) compressor->flushOut(fo, cprs_size);
-    }
+	void close() {
+		if (fo != nullptr) {
+			flush();
+			fclose(fo);
+			delete compressor;
+			fo = nullptr;
+			compressor = nullptr;			
+		}
+	}
 
-    void write(const Read& aread) {
-        if (compressor->needFlush(aread.size())) flush();
-        compressor->writeToBuffer('@');
-        compressor->writeToBuffer(aread.name.c_str(), aread.name.length());
-        if (!aread.comment.empty()) {
-            compressor->writeToBuffer(' ');
-            compressor->writeToBuffer(aread.comment.c_str(), aread.comment.length());
-        }
-        compressor->writeToBuffer('\n');
-        compressor->writeToBuffer(aread.seq.c_str(), aread.seq.length());
-        compressor->writeToBuffer('\n');
-        if (!aread.qual.empty()) {
-            compressor->writeToBuffer('+');
-            compressor->writeToBuffer('\n');
-            compressor->writeToBuffer(aread.qual.c_str(), aread.qual.length());
-            compressor->writeToBuffer('\n');
-        }
-    }
+	void flush() {
+		size_t cprs_size = compressor->compress();
+		if (cprs_size > 0) compressor->flushOut(fo, cprs_size);
+	}
 
-    void write(const std::string& line) {
-        if (compressor->needFlush(line.length())) flush();
-        compressor->writeToBuffer(line.c_str(), line.length());
-    }
+	void write(const Read& aread) {
+		if (compressor->needFlush(aread.size())) flush();
+		compressor->writeToBuffer('@');
+		compressor->writeToBuffer(aread.name.c_str(), aread.name.length());
+		if (!aread.comment.empty()) {
+			compressor->writeToBuffer(' ');
+			compressor->writeToBuffer(aread.comment.c_str(), aread.comment.length());
+		}
+		compressor->writeToBuffer('\n');
+		compressor->writeToBuffer(aread.seq.c_str(), aread.seq.length());
+		compressor->writeToBuffer('\n');
+		if (!aread.qual.empty()) {
+			compressor->writeToBuffer('+');
+			compressor->writeToBuffer('\n');
+			compressor->writeToBuffer(aread.qual.c_str(), aread.qual.length());
+			compressor->writeToBuffer('\n');
+		}
+	}
 
-    void write(char c) {
-        if (compressor->needFlush(1)) flush();
-        compressor->writeToBuffer(c);
-    }
+	void write(const std::string& line) {
+		if (compressor->needFlush(line.length())) flush();
+		compressor->writeToBuffer(line.c_str(), line.length());
+	}
+
+	void write(char c) {
+		if (compressor->needFlush(1)) flush();
+		compressor->writeToBuffer(c);
+	}
 };
 
-#endif //FQ_REIMPLEMENTED_GZIP_UTILS_HPP
+#endif
